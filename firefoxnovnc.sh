244cea0477a2af10172e8876892bdb3796bfce53
sonu536 <35693329+sonu536@users.noreply.github.com>
2026-08-09 11:05:36 +0530

    Update port for noVNC server from 5900 to 6080

---
+++ firefoxnovnc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+echo "updating system to successfully install required packages"
+sudo apt update
+echo "installing required packages"
+sudo apt install -y wget novnc websockify tigervnc-standalone-server tar openbox tilix dbus-x11
+sudo install -d -m 0755 /etc/apt/keyrings
+Import the Mozilla APT repository signing key:
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "The fingerprint should be 35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3. You may check it with the following command:"
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+echo "adding the Mozilla APT repository to your sources list:"
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo -e "Configuring APT to prioritize packages from the Mozilla repository:"
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+echo "updating and installing firefox"
+sudo apt-get update && sudo apt-get install firefox -y
+echo "firefox installed, now starting the novnc server"
+curl -fsSL https://raw.githubusercontent.com/GitXpresso/Browsers-NoVNC/refs/heads/main/firefoxdesktop | bash
+tigervncserver  -SecurityTypes none  --I-KNOW-THIS-IS-INSECURE -xstartup /usr/bin/openbox -geometry 1366x768 -localhost no :0
+websockify -D --web=/usr/share/novnc/  --cert=~/linux-novnc/novnc.pem 6080 localhost:6080
+export DISPlAY=:0
+firefox
+echo -e "novnc server started go https://localhost:6080"
